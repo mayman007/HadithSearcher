8bed20e08fe49495d61ad9aad5151bb7d299410d
Shinobi7k <medoems2210@gmail.com>
2023-07-28 01:32:16 +0300

    decreased timeout

--- lib/views/similar_hadith_view.dart
+++ lib/views/similar_hadith_view.dart
@@ -90,7 +90,7 @@ class _SimilarHadithViewState extends State<SimilarHadithView> {
     try {
       var url = Uri.parse(
           'https://dorar-hadith-api.cyclic.app/v1/site/hadith/similar/$hadithId');
-      var response = await http.get(url).timeout(const Duration(seconds: 32));
+      var response = await http.get(url).timeout(const Duration(seconds: 24));
       var decodedBody = utf8.decode(response.bodyBytes);
       var jsonResponse = json.decode(decodedBody);
 
